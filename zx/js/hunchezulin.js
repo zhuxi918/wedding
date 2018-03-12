769f405fd7e102ced7d4b8fee25d9ad2664ee413
zhuxi <zhuxi@163.com>
2018-03-12 10:32:26 +0800

    1

--- zx/js/hunchezulin.js
+++ zx/js/hunchezulin.js
@@ -13,12 +13,12 @@ $(
     })
    
 )
-$(//图片渐入渐出效果
+$(//图片渐入渐出效果 点击传入图片信息
      $('.picBrowser').click(e=>{
       $('.picBrowser').fadeOut()
     })
 )
-$(//视频渐入渐出效果
+$(//视频渐入渐出效果 点击传入视频信息
     $('.show-video').on('click',()=>{
         $('.videoBrowser').fadeIn(()=>{
             $('#videoPlayer')[0].play()
@@ -29,6 +29,21 @@ $(//视频渐入渐出效果
 $(
      $('.videoBrowser').click(e=>{
       $('.videoBrowser').fadeOut()
-      $('#videoPlayer')[0].pause    ()
+      $('#videoPlayer')[0].pause()
+    })
+)
+$(
+    $('.car-sPic img').on('click',e=>{
+       $('.car-mPic img').attr('src',$(e.target).attr('src'))
+    })
+)
+$(
+    $('.show-details span').click(e=>{
+        $('.carDetails-container').fadeIn()
+    })
+)
+$(
+    $('.showDetails .close').click(e=>{
+        $('.carDetails-container').fadeOut()
     })
 )
